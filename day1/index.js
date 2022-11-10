694811570f27f3f46a0e48cbd0917ef8eea727ac
Rafli Dewanto <raflidewanto97@gmail.com>
2022-11-10 23:08:10 +0700

    modified:   day1/index.html modified:   day1/index.js

--- day1/index.js
+++ day1/index.js
@@ -10,14 +10,24 @@ home.addEventListener("click", () => {
     about.classList.remove("active")
 })
 
-news.addEventListener("click", () => {
-    news.classList.add("active")
-    home.classList.remove("active")
-    contact.classList.remove("active")
-    about.classList.remove("active")
-})
+// news.addEventListener("click", () => {
+//     news.classList.add("active")
+//     home.classList.remove("active")
+//     contact.classList.remove("active")
+//     about.classList.remove("active")
+// })
 
-function myFunction() {
+const myFunction = () => {
     const home = document.querySelector(".home")
     home.classList.add("active");
 }
+
+// document.querySelector('.target').addEventListener('click', function (event) {
+//     event.preventDefault();
+//     var target = document.querySelector('.target');
+//     if (target.classList.contains('active')) {
+//         target.classList.remove('active');
+//     } else {
+//         target.classList.add('active');
+//     }
+// });
