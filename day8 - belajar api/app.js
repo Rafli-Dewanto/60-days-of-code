6023f540909c415f59569848d3f7393d450c21cf
Rafli Dewanto <raflidewanto97@gmail.com>
2022-11-27 10:06:21 +0700

    add day8

--- day8 - belajar api/app.js
+++ day8 - belajar api/app.js
@@ -1,23 +1,37 @@
-import apiKey from "apikey.js";
+require('dotenv').config()
 const express = require('express');
 const https = require('https');
-const PORT = 3000
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=${apiKey}`;
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", (req, res) => {
+    const kotaQueryUser = req.body.cityName;
+    const apiKey = process.env.API_KEY;
+    const unit = `metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${kotaQueryUser}&appid=${apiKey}&units=${unit}`;
     https.get(url, (response) => {
         response.on("data", (data) => {
             const weatherResult = JSON.parse(data);
-            const suhu = weatherResult.main.temp - 273;
+            const suhu = weatherResult.main.temp;
             const deskripsi = weatherResult.weather[0].description;
-            console.log(`Jakarta: ${suhu.toFixed(2)} derajat celcius`);
-            console.log(`${deskripsi}`);
-        })
-    })
-    res.send("server ready")
-})
+            const iconPath = weatherResult.weather[0].icon;
+            const iconURL = `http://openweathermap.org/img/wn/${iconPath}@2x.png`;
+            
+            res.write(`<h1>Suhu di ${kotaQueryUser}: ${suhu.toFixed(1)} derajat celcius</h1>`);
+            res.write(`<p>Deskripsi: ${deskripsi}</p>`)
+            res.write(`<img src="${iconURL}" alt="icon-cuaca">`)
+            res.send();
+        });
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
-})
+});
